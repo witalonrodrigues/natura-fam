d71f1efef8de781f53e498fb6235476738782ca0
witalonrodrigues <witalonar@outlook.com>
2025-10-27 15:49:22 -0300

    Corrige vite.config.js para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/natura-fam/'
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig(({ mode }) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  // ----------------------------------------------------
+
+  return {
+    base: '/natura-fam/',
     
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+    server: {
+      host: "::",
+      port: 8080,
+      // Removido 'base' daqui
+    },
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        // Agora o '__dirname' está definido e funciona
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
