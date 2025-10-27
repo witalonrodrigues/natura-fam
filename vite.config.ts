ee15c907d50488d4d300c6eff0eae8a4a818c9c6
witalonrodrigues <witalonar@outlook.com>
2025-10-27 15:47:26 -0300

    Corrige vite.config.js para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/natura-fam/'
   server: {
     host: "::",
     port: 8080,
-    base: '/natura-fam/'
+    
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
